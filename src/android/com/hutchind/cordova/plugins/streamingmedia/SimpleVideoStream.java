876fb333daab5d1521613d7549fc6b9322d41305
utgMarketing <88131988+utgMarketing@users.noreply.github.com>
2023-02-05 12:40:17 +0000

    Update SimpleVideoStream.java

--- src/android/com/hutchind/cordova/plugins/streamingmedia/SimpleVideoStream.java
+++ src/android/com/hutchind/cordova/plugins/streamingmedia/SimpleVideoStream.java
@@ -42,17 +42,7 @@ MediaPlayer.OnErrorListener, MediaPlayer.OnBufferingUpdateListener {
 		this.requestWindowFeature(Window.FEATURE_NO_TITLE);
 		this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 		
-		// VISIV START
-		if(Build.VERSION.SDK_INT > 11 && Build.VERSION.SDK_INT < 19) { // lower api
-			View v = this.getWindow().getDecorView();
-			v.setSystemUiVisibility(View.GONE);
-		} else if(Build.VERSION.SDK_INT >= 19) {
-			//for new api versions.
-			View decorView = this.getWindow().getDecorView();
-			int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
-			decorView.setSystemUiVisibility(uiOptions);
-		}
-		// VISIV STOP
+		hideUi();
 
 		Bundle b = getIntent().getExtras();
 		mVideoUrl = b.getString("mediaUrl");
@@ -104,6 +94,20 @@ MediaPlayer.OnErrorListener, MediaPlayer.OnBufferingUpdateListener {
 		setContentView(relLayout, relLayoutParam);
 
 		play();
+	}
+	
+	private void hideUi() {
+	     	// VISIV START
+		if(Build.VERSION.SDK_INT > 11 && Build.VERSION.SDK_INT < 19) { // lower api
+			View v = this.getWindow().getDecorView();
+			v.setSystemUiVisibility(View.GONE);
+		} else if(Build.VERSION.SDK_INT >= 19) {
+			//for new api versions.
+			View decorView = this.getWindow().getDecorView();
+			int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
+			decorView.setSystemUiVisibility(uiOptions);
+		}
+		// VISIV STOP
 	}
 
 	private void play() {
@@ -239,6 +243,9 @@ MediaPlayer.OnErrorListener, MediaPlayer.OnBufferingUpdateListener {
 
 	@Override
 	public boolean onTouchEvent(MotionEvent event) {
+		
+		hideUi();
+		
 		if (mMediaController != null)
 			mMediaController.show();
 		        close.setVisibility(View.VISIBLE);
